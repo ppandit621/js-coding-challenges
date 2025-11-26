717c48f575cf08c553b470c1f3ab9c0634d3c0d1
Pankaj <ppandit524@gmail.com>
2025-11-26 22:32:21 +0545

    Solved q1: Delayed Greeting

---
+++ AsynchronousJavascript/q1.js
@@ -0,0 +1,10 @@
+// Q1. Delayed Greeting
+// Write delayedHello() that logs "Hello" after 2 seconds using setTimeout .
+
+function delayedHello() {
+  setTimeout(() => {
+    console.log("Hello");
+  }, 2000); 
+}
+
+delayedHello();
