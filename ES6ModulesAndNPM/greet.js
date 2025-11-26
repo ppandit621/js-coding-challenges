80dccb1f5c7a294f16a7a26e8b58c919ec54ca9f
Pankaj <ppandit524@gmail.com>
2025-11-26 23:04:25 +0545

    Solved q2: Default Export

---
+++ ES6ModulesAndNPM/greet.js
@@ -0,0 +1,7 @@
+// Q2. Default Export
+// Create greet.js exporting a default function that returns "Hello &lt;name&gt;" . Import it
+// as g in main.js .
+
+export default function greet(name) {
+  return `Hello ${name}`;
+}
--- ES6ModulesAndNPM/main.js
+++ ES6ModulesAndNPM/main.js
@@ -1,4 +1,7 @@
 import { add, sub } from './math.js';
+import g from './greet.js';
 
 console.log(add(5, 3)); // 8
 console.log(sub(5, 3)); // 2
+
+console.log(g("Pankaj"));
