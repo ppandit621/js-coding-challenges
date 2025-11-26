c7af5f3582ff2265a639265dc343e922bc798052
Pankaj <ppandit524@gmail.com>
2025-11-26 21:39:07 +0545

    Solved q2:Template Literal

---
+++ JavaScriptBasics/q2.js
@@ -0,0 +1,10 @@
+// Template Literal Formatter
+
+// Write a function formatUser(name, age) that returns: "User &lt;name&gt; is &lt;age&gt; years old"
+// Use template literals.
+
+function formatUser(name, age) {
+  return `User ${name} is ${age} years old` ;
+}
+
+console.log(formatUser("Hari", 30)); 
